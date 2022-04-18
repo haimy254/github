c7f50327dfbf38ec38eaf23389aa819279a9f6ab
haimy254 <haimanaare@gmail.com>
2022-04-18 05:30:18 +0300

    routing

--- Git-hub/src/app/app-routing.module.ts
+++ Git-hub/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import {Profile} from './profile';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , component: SearchComponent},
+  {path: 'navbar' , component:NavbarComponent},
+  { path: 'profile', component: ProfileComponent },
+{path:'search' , component:SearchComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
